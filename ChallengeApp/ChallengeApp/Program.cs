5ef7117d7fa13820344f362ccafd88a5ab690ba5
Wieslaw96 <w.kaminski@vp.pl>
2023-09-20 19:24:33 +0200

    Zadanie dzień 4

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string name = "Ewa";
+string sex = "Kobieta";
+int age = 33;
+
+if (sex == "Kobieta" && age < 30)
+{
+    Console.WriteLine("Kobieta poniżej 30 lat");
+}
+else if (name == "Ewa" && age == 33)
+{
+    Console.WriteLine("Ewa, lat 33");
+}
+else if(sex == "Mężczyzna" && age < 18)
+{
+    Console.WriteLine("Niepełnoletni Mężczyzna");
+}
